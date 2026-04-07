041735b01e102a3336186e351aad15a0349a676b
JanviSangani <106907842+JanviSangani@users.noreply.github.com>
2026-04-07 13:33:13 +0530

    Create Order API changes

--- orders_controller.rb
+++ orders_controller.rb
@@ -3,13 +3,24 @@ class ApplicationController < ApplicationController
     begin
     #check for required params here.
     unless params[:external_id].present? && params[:placed_at].present? && params[:line_items].present?
-      {status: 801, success: false, message: "Required params are missing"}
+      return {status: 801, success: false, message: "Required params are missing"}
+    end
+
+    already_exist = Order.find_by(external_id: params[:external_id])
+    if already_exist
+      if already_exist.locked_at.present? || already_exist.locked_at > Time.now - 15.minutes
+        return {status: 801, success: false, message: "Locked for Edit"}
+      end
     end
 
     order = Order.create!(external_id: params[:external_id], placed_at: DateTime.now)
     if order.persist
-
+      params[:line_items]&.each do |line_item|
+        order.line_items.create(sku: line_item[:sku], quantity: line_item[:quantity], original: true)
+      end
     end
+    return {status: 200, success: true, message: "Order Created"}
+
     rescue StandardError => e
       errors.add({status: 801, success: false, message: e.message})
     end
